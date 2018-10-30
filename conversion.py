f3ed420949974be2b566fda1ff90a25317e16f7a
Aidan White <aidan.white36@gmail.com>
2018-10-30 14:58:15 -0500

    Update conversion script to correct format

--- conversion.py
+++ conversion.py
@@ -1,6 +1,8 @@
 '''
     Convert csv file to format:
-    'word1 word2 word3 __label__l1 __label__l2 __label__l3'
+    word1 __label__l1 
+    word2 __label__l2
+    word3 __label__l3
 '''
 
 import csv
@@ -12,19 +14,14 @@ def main():
     docs = []
     labels = []
 
-    with open(input_file, "r") as f:
+    with open(input_file, mode="r", encoding="utf-8") as f:
         reader = csv.reader(f)
         for row in reader:
             docs.append(row[1])
             labels.append(row[2])
 
-    with open(output_file, "w") as f:
-        for word in docs:
-            f.write(word + " ")
-
-        f.write(" ")
-
-        for label in labels:
-            f.write("__label__" + label)
+    with open(output_file, "w", encoding="utf-8") as f:
+        for i in range(len(docs)):
+            f.write("{0} __label__{1}\n".format(docs[i].replace("\n", ""), labels[i]))
 
 main()
