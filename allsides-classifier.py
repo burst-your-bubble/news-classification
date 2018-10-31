4ef3721d09551a465b88bf66fa496b2ac38470da
Aidan White <aidan.white36@gmail.com>
2018-10-31 00:01:43 -0500

    Use more evaluation metrics for the classifier

--- allsides-classifier.py
+++ allsides-classifier.py
@@ -1,7 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 import numpy as np
 import pandas as pd
 import csv
@@ -11,8 +13,7 @@ file_path = "./data/allsides.csv"
 n_grams = 1
 train_pct = 0.8
 
-
-def main():
+def load_data():
     csv.field_size_limit(sys.maxsize)
     docs = []
     labels = []
@@ -29,9 +30,13 @@ def main():
             elif row[2] == "c":
                 docs.append(row[1])
                 labels.append(2)
-
     docs = np.array(docs)
     labels = np.array(labels)
+
+    return docs, labels
+
+def main():
+    docs, labels = load_data()
 
     train_split = int(train_pct*(len(docs)))
     train_data = docs[:train_split]
@@ -39,24 +44,18 @@ def main():
     test_data = docs[train_split:]
     test_labels = labels[train_split:]
 
-    count_vect = CountVectorizer(ngram_range=(1,n_grams))
-    tfidf_transformer = TfidfTransformer()    
-    clf = LinearSVC()
+    clf = Pipeline([('vect', CountVectorizer()),
+                    ('tfidf', TfidfTransformer()),
+                    ('clf', LinearSVC())])
+    clf.fit(train_data, train_labels)
 
-    train_counts = count_vect.fit_transform(train_data)
-    train_tfidf = tfidf_transformer.fit_transform(train_counts)
+    predicted = clf.predict(test_data)
 
-    clf.fit(train_tfidf, train_labels)
-
-    test_counts = count_vect.transform(test_data)
-    test_tfidf = tfidf_transformer.transform(test_counts)
-
-    predicted = clf.predict(test_tfidf)
-    
-
-    # Evaluate accuracy of model
+    # Evaluate the model
     acc = accuracy_score(test_labels, predicted)
     print("Accuracy Score: {}".format(acc))
+    report = classification_report(test_labels, predicted)
+    print(report)
 
 if __name__ == "__main__":
     main()
